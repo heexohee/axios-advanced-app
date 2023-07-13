28bad6b6ff0cd75f2c2ca18f116650e842bf56d8
heexohee <rsoy2918@naver.com>
2023-07-13 20:22:09 +0900

    온체인지 벨류로 인풋과 버튼 묶기

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import axios from "axios";
 
 function App() {
   const [todos, setTodos] = useState(null);
+  const [inputValue, setInputValue] = useState({
+    title:'', 
+  })
+ // nosql, mongodb, json 등은 id는 자동으로 부여하기 때문에
+ // 우리는 타이틀만 갖고 있으면 된다!(db.json파일 참조)
 
   const fetchTodos = async () => {
     // await가 없으면 응답을 받기전에 콘솔로그레 response가 찍혀서 pending 으로 뜸.
@@ -19,18 +24,40 @@ function App() {
   }, []);
 
   return (
-    <div>
-      {/* ?로 옵셔널 체이닝 */}
-      {todos?.map((item) => {
-        return ( 
-          // 맵 함수 돌아갈 땐, 항상 최상위 태그에 key라는 프로퍼티가 필요함. 
-          // 몇번 째 요소인지 알려주기 위해서!
-          <div key={item.id}>
-            {item.id} : {item.title}
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div>
+        
+        {/* 인풋 영역 */}
+      <form>
+        <input type="text"
+        value={inputValue.title}
+        onChange={(e)=>{
+          setInputValue({
+            title : e.target.value
+          });
+        }}
+        />
+        <button>추가</button>
+        
+        
+      </form>
+      </div>
+
+      <div>
+        {/* 데이터 영역 */}
+
+        {/* ?로 옵셔널 체이닝 */}
+        {todos?.map((item) => {
+          return (
+            // 맵 함수 돌아갈 땐, 항상 최상위 태그에 key라는 프로퍼티가 필요함.
+            // 몇번 째 요소인지 알려주기 위해서!
+            <div key={item.id}>
+              {item.id} : {item.title}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
